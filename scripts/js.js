978195f618b1e68fbd872e5380e169362c92b3af
Ignacio Lapolla <ignacio.lapolla@ibm.com>
2024-11-04 20:50:22 -0300

    pagina contacto

--- scripts/js.js
+++ scripts/js.js
@@ -21,3 +21,15 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+fetch("navbar.html")
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("No se pudo cargar el navbar");
+        }
+        return response.text();
+    })
+    .then(data => {
+        document.getElementById("navbar").innerHTML = data;
+    })
+    .catch(error => console.error("Error al cargar el navbar:", error));
