4aecff701f3a9ad27fd8d48053cb0b6fea8e9069
bau <bau@iMac-Pro.localdomain>
2024-11-06 17:18:38 -0300

    se agregaron los sliders y forms en reservas

---
+++ scripts/swiper.js
@@ -0,0 +1,27 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        450: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        769: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+        },
+        1240: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+        },
+    } 
+});
